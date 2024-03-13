2cc4e03255a2c16f516c4e35dd965a00ad9b73b1
SEAWEED\lucas <lucas.koh@hotmail.com>
2024-03-14 00:22:58 +0800

    added search filters

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -30,9 +30,35 @@
         <li class="nav-item">   
             <form action="../process_search.php" method="post">
                 <input maxlength="55" type="text" class="form-control" id="search" name="search" placeholder="Search for keywords">
+                <select name="cat">
+                    <option value="all_cats_in_db">All categories</option>
+                    <?php
+                        $config = parse_ini_file('/var/www/private/db-config.ini');
+                        $conn = new mysqli(
+                            $config['servername'],
+                            $config['username'],
+                            $config['password'],
+                            $config['dbname']
+                        );
+
+                        $stmt = $conn->prepare("SELECT * FROM product_category");
+                        $stmt->execute();
+                        $result = $stmt->get_result();
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<option value='".$row['cat_id']."'>".$row['cat_name']."</option>";
+                            }
+                        } else{
+                            echo "<option value=''>No categories found</option>";
+                        }
+                        $stmt->close();
+                        $conn->close();
+                    ?>
+                </select>
                 <button type="submit">Submit</button>
             </form>
         </li>
+
         </ul>
         </div>
         </div>
