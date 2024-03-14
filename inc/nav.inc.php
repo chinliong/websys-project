6b32f44c6b52f8d27ca2a9c6b1b34013677fa8b3
chinliong <116244626+chinliong@users.noreply.github.com>
2024-03-14 10:41:00 +0800

    updated navbar and changed session placement into navbar

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
     <nav class="navbar navbar-expand-sm bg-secondary" data-bs-theme="dark">
         <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="images/logo.jpg"/></a>
@@ -9,7 +12,27 @@
         <li class="nav-item">
             <a class="nav-link active" href="../index.php">Home</a>
         </li>
-        <li class="nav-item">
+        <ul class="navbar-nav ms-auto">
+            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) : ?>
+                <li class="nav-item">
+                <a class="nav-link" href="../new_listing.php">New Listing</a>
+                </li>
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="../profile.php"><i class="fas fa-user"></i> <?php echo htmlspecialchars($_SESSION['username']); ?></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../logout.php"><i class="fas fa-sign-out-alt d-none d-lg-inline"></i> Logout <span class="d-inline d-lg-none"> (<?php echo htmlspecialchars($_SESSION['uname']); ?>)</span></a>
+                </li>
+            <?php else : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="../register.php"><i class="fas fa-user-plus d-none d-lg-inline"></i> Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../login.php"><i class="fas fa-sign-in-alt d-none d-lg-inline"></i> Login</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+        <!-- <li class="nav-item">
             <a class="nav-link" href="../new_listing.php">New Listing</a>
         </li>
         <li class="nav-item">
@@ -26,7 +49,7 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" href="../new_listing.php">new_listing</a>
-        </li>
+        </li> -->
         <li class="nav-item">   
             <form action="../process_search.php" method="post">
                 <input maxlength="55" type="text" class="form-control" id="search" name="search" placeholder="Search for keywords">
