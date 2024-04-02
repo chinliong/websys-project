98633df32e9f0b998bc94f173f91ea74991bf232
SEAWEED\lucas <lucas.koh@hotmail.com>
2024-04-02 18:38:28 +0800

    updated login front end

--- login.php
+++ login.php
@@ -27,17 +27,17 @@
 
   <main>
     <section id="login">
-    <div class="login-container d-flex justify-content-center align-items-center min-vh-100">
+
       <section>
-        <div class="login-form card p-4 black-words">
-          <h2 class="text-center mb-4 black-words">Login to Your Account</h2>
+        <div class="row">
+          <h2 class="text-center mb-4">Login to Your Account</h2>
           <form action="process_login.php" method="post">
-            <div class="form-group">
-              <label for="email" class="form-label black-words">Email</label>
+            <div class="form-group col-md-6 col-sm-12">
+              <label for="email" class="form-label">Email</label>
               <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
             </div>
-            <div class="form-group">
-              <label for="pwd" class="form-label black-words">Password</label>
+            <div class="form-group col-md-6 col-sm-12">
+              <label for="pwd" class="form-label">Password</label>
               <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter your password" required>
             </div>
             <div class="form-check mb-3">
@@ -46,13 +46,12 @@
             </div>
             <div class="g-recaptcha" data-sitekey="6LfQNJ8pAAAAAJnPvUiOcFTvlB2a2N2xbPZbhC5e"></div>
 
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-primary submitt-btn">Login</button>
             <a href="#" class="forgot-password">Forgot Password?</a>
           </form>
           <p class="mt-3 text-center">Don't have an account? <a href="register.php">Sign Up</a></p>
-      </section>
     </div>
-    </div>
+    </section>
 
     <script>
       // Define onSubmit function to handle reCAPTCHA verification
@@ -69,4 +68,4 @@
   </main>
 </body>
 
-</html
+</html>
