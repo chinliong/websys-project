d97e08c08f1732a220eb6f55359e6d8f5356bdb4
Aravind699 <aravindraj754@yahoo.com.sg>
2024-04-04 18:26:52 +0800

    live chat

--- index.php
+++ index.php
@@ -178,6 +178,17 @@ session_start();
             }
         });
     </script>
+        <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/660e7de6a0c6737bd1285037/1hqk9njno';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
     <footer>
         <p><em>Copyright &copy; 2024 Little Haven Shoppe Pte. Ltd.</em></p>
     </footer>
